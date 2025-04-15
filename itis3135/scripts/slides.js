fdf77b42346de4d7ea52e3270392ad927c6aafc5
graceashton3 <mashton1@charlotte.edu>
2025-04-14 21:00:48 -0400

    Update slides.js

--- itis3135/scripts/slides.js
+++ itis3135/scripts/slides.js
@@ -5,8 +5,9 @@ function showSlides(n) {
   const slides = document.getElementsByClassName("mySlides");
   const dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length); {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) 
+    {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
