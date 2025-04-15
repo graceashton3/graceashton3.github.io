2bfbc5393685b7933413840a7dfe767cc78c05a8
graceashton3 <mashton1@charlotte.edu>
2025-04-14 20:13:20 -0400

    Update slides.js

--- itis3135/scripts/slides.js
+++ itis3135/scripts/slides.js
@@ -1,5 +1,4 @@
 let slideIndex = 1;
-var showSlides;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -17,15 +16,23 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
