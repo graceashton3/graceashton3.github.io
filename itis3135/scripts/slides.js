f5ba0cf004e347eac452918555aa6ccaf663ef82
graceashton3 <mashton1@charlotte.edu>
2025-04-14 20:56:11 -0400

    Update slides.js

--- itis3135/scripts/slides.js
+++ itis3135/scripts/slides.js
@@ -1,18 +1,21 @@
 let slideIndex = 1;
+showSlides(slideIndex);
 
-// Function declaration first
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -22,21 +25,8 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-}
-
-// Then call it
-showSlides(slideIndex);
-
-// Navigation functions
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
 
 
